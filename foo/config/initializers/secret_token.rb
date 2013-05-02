b68bea7939c68380797b3b1f81e0a38bc72dea19
root <root@openam.sophia.local>
2013-05-02 14:10:16 +0900

    testup

---
+++ foo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foo::Application.config.secret_token = '1b3f64ae4af67b9099ac6f078bc1e4b715c8541736cebedfafa9fb86fcb5da96b14bb6f49575485224d51a6cd1fc222872f97bb3acf6651909445de368d30fe7'
